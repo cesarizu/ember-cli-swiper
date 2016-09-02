f5b6f9b6c9adf50945901f56848aeab6a956b848
César Izurieta <cesar@caih.org>
2016-09-02 22:54:17 +0200

    Updated ember-cli to v2.11.0

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const { Router: ERouter } = Ember;
-
-const Router = ERouter.extend({
+const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
