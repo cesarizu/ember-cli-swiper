3b47f0f5c2130e18f031d007c2cd1422b99b9a9c
César Izurieta <cesar@caih.org>
2016-09-03 01:12:17 +0200

    Allow all swiper options

--- addon/components/swiper-container.js
+++ addon/components/swiper-container.js
@@ -3,6 +3,174 @@
 import Ember from 'ember';
 import layout from '../templates/components/swiper-container';
 
+const params = Object.keys({
+  // The follwing parameters are copied verbatim from idangerous.swiper.js
+  direction: 'horizontal',
+  touchEventsTarget: 'container',
+  initialSlide: 0,
+  speed: 300,
+  // autoplay
+  autoplay: false,
+  autoplayDisableOnInteraction: true,
+  autoplayStopOnLast: false,
+  // To support iOS's swipe-to-go-back gesture (when being used in-app, with UIWebView).
+  iOSEdgeSwipeDetection: false,
+  iOSEdgeSwipeThreshold: 20,
+  // Free mode
+  freeMode: false,
+  freeModeMomentum: true,
+  freeModeMomentumRatio: 1,
+  freeModeMomentumBounce: true,
+  freeModeMomentumBounceRatio: 1,
+  freeModeSticky: false,
+  freeModeMinimumVelocity: 0.02,
+  // Autoheight
+  autoHeight: false,
+  // Set wrapper width
+  setWrapperSize: false,
+  // Virtual Translate
+  virtualTranslate: false,
+  // Effects
+  effect: 'slide', // 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
+  coverflow: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true
+  },
+  flip: {
+      slideShadows : true,
+      limitRotation: true
+  },
+  cube: {
+      slideShadows: true,
+      shadow: true,
+      shadowOffset: 20,
+      shadowScale: 0.94
+  },
+  fade: {
+      crossFade: false
+  },
+  // Parallax
+  parallax: false,
+  // Scrollbar
+  scrollbar: null,
+  scrollbarHide: true,
+  scrollbarDraggable: false,
+  scrollbarSnapOnRelease: false,
+  // Keyboard Mousewheel
+  keyboardControl: false,
+  mousewheelControl: false,
+  mousewheelReleaseOnEdges: false,
+  mousewheelInvert: false,
+  mousewheelForceToAxis: false,
+  mousewheelSensitivity: 1,
+  // Hash Navigation
+  hashnav: false,
+  // Breakpoints
+  breakpoints: undefined,
+  // Slides grid
+  spaceBetween: 0,
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  slidesPerColumnFill: 'column',
+  slidesPerGroup: 1,
+  centeredSlides: false,
+  slidesOffsetBefore: 0, // in px
+  slidesOffsetAfter: 0, // in px
+  // Round length
+  roundLengths: false,
+  // Touches
+  touchRatio: 1,
+  touchAngle: 45,
+  simulateTouch: true,
+  shortSwipes: true,
+  longSwipes: true,
+  longSwipesRatio: 0.5,
+  longSwipesMs: 300,
+  followFinger: true,
+  onlyExternal: false,
+  threshold: 0,
+  touchMoveStopPropagation: true,
+  // Unique Navigation Elements
+  uniqueNavElements: true,
+  // Pagination
+  pagination: null,
+  paginationElement: 'span',
+  paginationClickable: false,
+  paginationHide: false,
+  paginationBulletRender: null,
+  paginationProgressRender: null,
+  paginationFractionRender: null,
+  paginationCustomRender: null,
+  paginationType: 'bullets', // 'bullets' or 'progress' or 'fraction' or 'custom'
+  // Resistance
+  resistance: true,
+  resistanceRatio: 0.85,
+  // Next/prev buttons
+  nextButton: null,
+  prevButton: null,
+  // Progress
+  watchSlidesProgress: false,
+  watchSlidesVisibility: false,
+  // Cursor
+  grabCursor: false,
+  // Clicks
+  preventClicks: true,
+  preventClicksPropagation: true,
+  slideToClickedSlide: false,
+  // Lazy Loading
+  lazyLoading: false,
+  lazyLoadingInPrevNext: false,
+  lazyLoadingInPrevNextAmount: 1,
+  lazyLoadingOnTransitionStart: false,
+  // Images
+  preloadImages: true,
+  updateOnImagesReady: true,
+  // loop
+  loop: false,
+  loopAdditionalSlides: 0,
+  loopedSlides: null,
+  // Control
+  control: undefined,
+  controlInverse: false,
+  controlBy: 'slide', //or 'container'
+  // Swiping/no swiping
+  allowSwipeToPrev: true,
+  allowSwipeToNext: true,
+  swipeHandler: null, //'.swipe-handler',
+  noSwiping: true,
+  noSwipingClass: 'swiper-no-swiping',
+  // NS
+  slideClass: 'swiper-slide',
+  slideActiveClass: 'swiper-slide-active',
+  slideVisibleClass: 'swiper-slide-visible',
+  slideDuplicateClass: 'swiper-slide-duplicate',
+  slideNextClass: 'swiper-slide-next',
+  slidePrevClass: 'swiper-slide-prev',
+  wrapperClass: 'swiper-wrapper',
+  bulletClass: 'swiper-pagination-bullet',
+  bulletActiveClass: 'swiper-pagination-bullet-active',
+  buttonDisabledClass: 'swiper-button-disabled',
+  paginationCurrentClass: 'swiper-pagination-current',
+  paginationTotalClass: 'swiper-pagination-total',
+  paginationHiddenClass: 'swiper-pagination-hidden',
+  paginationProgressbarClass: 'swiper-pagination-progressbar',
+  // Observer
+  observer: false,
+  observeParents: false,
+  // Accessibility
+  a11y: false,
+  prevSlideMessage: 'Previous slide',
+  nextSlideMessage: 'Next slide',
+  firstSlideMessage: 'This is the first slide',
+  lastSlideMessage: 'This is the last slide',
+  paginationBulletMessage: 'Go to slide {{index}}',
+  // Callbacks
+  runCallbacksOnInit: true
+});
+
 const { Component, computed, observer, on, run, $ } = Ember;
 
 export default Component.extend({
@@ -10,20 +178,20 @@ export default Component.extend({
   classNames: ['swiper-container'],
   swiper: false,
 
-  swiperOptions: computed('pagination', 'loop', 'vertical', 'onlyExternal', function() {
+  swiperOptions: computed.apply(Ember, params.concat('pagination', 'navigation', 'vertical', 'centered', function() {
     let options = {};
 
-    if (this.get('pagination')) {
+    params.forEach((param) => {
+      if (this.get(param) !== undefined) {
+        options[param] = this.get(param);
+      }
+    });
+
+    if (this.get('pagination') === true) {
       options.pagination = `#${this.get('elementId')} .swiper-pagination`;
-      options.paginationClickable = true;
-    }
-
-    if (this.get('nextButton')) {
-      options.nextButton = this.get('nextButton');
-    }
-
-    if (this.get('prevButton')) {
-      options.prevButton = this.get('prevButton');
+      if (this.get('paginationClickable') !== false) {
+        options.paginationClickable = true;
+      }
     }
 
     if (this.get('navigation')) {
@@ -31,80 +199,18 @@ export default Component.extend({
       options.prevButton = '.swiper-button-prev';
     }
 
-    if (this.get('loop')) {
-      options.loop = true;
-    }
-
-    if (this.get('initialSlide')) {
-      options.initialSlide = this.get('initialSlide');
-    }
-
-    // Disables swiping
-    if (this.get('followFinger')) {
-      options.followFinger = false;
-    }
-
-    // disable all user interactions
-    if (this.get('onlyExternal')) {
-      options.onlyExternal = true;
-    }
-
     if (this.get('vertical')) {
       options.direction = 'vertical';
-    }
-
-    if (this.get('slidesPerGroup')) {
-      options.slidesPerGroup = this.get('slidesPerGroup');
-    }
-
-    if (this.get('slidesPerView')) {
-      options.slidesPerView = this.get('slidesPerView');
-    }
-
-    if (this.get('spaceBetween')) {
-      options.spaceBetween = this.get('spaceBetween');
     }
 
     if (this.get('centered')) {
       options.centeredSlides = true;
     }
 
-    if (this.get('freeMode')) {
-      options.freeMode = true;
-    }
-
-    if (this.get('speed')) {
-      options.speed = this.get('speed');
-    }
-
-    if (this.get('freeModeSticky')) {
-      options.freeModeSticky = true;
-    }
-
-    if (this.get('grabCursor')) {
-      options.grabCursor = true;
-    }
-
-    if (this.get('breakpoints')) {
-      options.breakpoints = this.get('breakpoints');
-    }
-
-    if (this.get('autoplay')) {
-      options.autoplay = this.get('autoplay');
-    }
-
-    if (this.get('autoplayStopOnLast')) {
-      options.autoplayStopOnLast = this.get('autoplayStopOnLast');
-    }
-
-    if (this.get('autoplayDisableOnInteraction')) {
-      options.autoplayDisableOnInteraction = this.get('autoplayDisableOnInteraction');
-    }
-
     options.onSlideChangeEnd = this.slideChanged.bind(this);
 
     return options;
-  }),
+  })),
 
   updateTriggered: observer('updateFor', function() {
     run.once(this, this.get('swiper').update);
@@ -143,8 +249,8 @@ export default Component.extend({
 
   initSwiper: on('didInsertElement', function() {
     run.scheduleOnce('afterRender', this, function() {
-      this.set('swiper', new Swiper(`#${this.get('elementId')}`, this.get('swiperOptions')));
-      this.set('registerAs', this);
+    this.set('swiper', new Swiper(`#${this.get('elementId')}`, this.get('swiperOptions')));
+    this.set('registerAs', this);
     });
   })
 });
